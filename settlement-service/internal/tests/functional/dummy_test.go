4e883677c6dabf656bfed006bd049865262ded0f
arpusauri <aryapurnama877@gmail.com>
2026-05-12 23:36:18 +0700

    fix settlement #2

---
+++ settlement-service/internal/tests/functional/dummy_test.go
@@ -0,0 +1,13 @@
+// tests/functional/dummy_test.go
+
+//go:build functional
+// +build functional
+
+package functional_test
+
+import "testing"
+
+// TestDummy memastikan perintah go test tidak gagal karena folder kosong
+func TestDummy(t *testing.T) {
+    // Bisa diisi dengan test sesungguhnya nanti
+}
